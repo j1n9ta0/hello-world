dcc3dc4451a713675fd2ee080b3ed8fe8cf0f6ae
jing_tao@outlook.com <j1ngta0>
2023-09-22 09:42:51 +0000

    优化ubuntu-server 22.04配置脚本

--- shell/ubuntu2204_init.sh
+++ shell/ubuntu2204_init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt purge iptables nano screen ufw -y
-sudo sed -i 's#http://archive.ubuntu.com/ubuntu/#http://cn.archive.ubuntu.com/ubuntu/#g' /etc/apt/sources.list
-sudo apt update
-sudo apt full-upgrade -y
-sudo apt autoremove -y
+apt purge iptables nano screen ufw -y
+sed 's#archive.ubuntu.com#cn.archive.ubuntu.com/#g' /etc/apt/sources.list.curtin.old >/etc/apt/sources.list
+apt update
+apt full-upgrade -y
+apt autoremove -y
 
-sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
-sudo resize2fs /dev/ubuntu-vg/ubuntu-lv
-sudo lvs
+lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
+resize2fs /dev/ubuntu-vg/ubuntu-lv
+lvs
 
 default_eth=$(ip route | awk 'NR==1{print $5}')
 arr_eth_ip4_addr=($(ip -f inet addr show dev $default_eth | grep -oP '(?<=inet\s)\d+(\.\d+){3}'))
@@ -28,14 +28,14 @@ gateway4=${gateway4:-$default_gateway4}
 ip4=${ip4:-${arr_eth_ip4_addr[0]}}
 dns4=${dns4:-$default_gateway4}
 
-sudo netplan set ethernets.$eth.dhcp4="NULL"
-sudo netplan set ethernets.$eth.addresses="NULL"
-sudo netplan set ethernets.$eth.addresses="[$ip4/24]"
-sudo netplan set ethernets.$eth.routes="[{to: default, via: $gateway4}]"
-sudo netplan set ethernets.$eth.nameservers.addresses="NULL"
-sudo netplan set ethernets.$eth.nameservers.addresses="[$dns4]"
+netplan set ethernets.$eth.dhcp4="NULL"
+netplan set ethernets.$eth.addresses="NULL"
+netplan set ethernets.$eth.gateway4="NULL"
+netplan set ethernets.$eth.routes="NULL"
+netplan set ethernets.$eth.nameservers.addresses="NULL"
+netplan set ethernets.$eth.addresses="[$ip4/24]"
+netplan set ethernets.$eth.routes="[{to: default, via: $gateway4}]"
+netplan set ethernets.$eth.nameservers.addresses="[$dns4]"
 # netplan set ethernets.$eth.optional=false
-sudo netplan apply
-sudo netplan get
-
-
+netplan apply
+netplan get
